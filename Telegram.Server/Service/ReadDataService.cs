edd90e8471e14d33c0b58922ab12996f866cd48a
Abdumannon Shamsiyev <0605AbMu@gmail.com>
2023-06-23 15:03:36 +0500

    Read and write data service abstraction implemented

--- Telegram.Server/Service/ReadDataService.cs
+++ Telegram.Server/Service/ReadDataService.cs
@@ -1,6 +1,24 @@
 namespace Telegram.Server.Service;
 
-public class ReadDataService
+public class ReadDataService : IReadDataService
 {
-    
+    public void LoadUserServiceData()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LoadClientServiceData()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LoadChatServiceData()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LoadMessageServiceData()
+    {
+        throw new NotImplementedException();
+    }
 }
--- Telegram.Server/Service/WriteDataService.cs
+++ Telegram.Server/Service/WriteDataService.cs
@@ -1,5 +1,24 @@
 namespace Telegram.Server.Service;
 
-public class WriteDataService
+public class WriteDataService : IWriteDataService 
 {
+    public void SaveClientServiceData()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SaveUserServiceData()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SaveChatServiceData()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SaveMessageServiceData()
+    {
+        throw new NotImplementedException();
+    }
 }
