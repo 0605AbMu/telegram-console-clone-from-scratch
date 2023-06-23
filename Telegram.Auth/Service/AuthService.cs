45edb1f531fba07b27fc7b6f8e14c4e94ae00207
Abdumannon Shamsiyev <0605AbMu@gmail.com>
2023-06-23 12:20:32 +0500

    TelegramClient.Auth project restructured

---
+++ Telegram.Auth/Service/AuthService.cs
@@ -0,0 +1,24 @@
+﻿using TelegramClient.Auth.Auth;
+using TelegramClient.Auth.Domain;
+using TelegramClient.Auth.Service.Interface;
+
+namespace TelegramClient.Auth.Service;
+
+public class AuthService : IAuthService
+{
+    private readonly IUserService _userService;
+
+    public AuthService(IUserService userService)
+    {
+        _userService = userService;
+    }
+    public User Login(string phoneNumber, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Registration(string name, string password, string phoneNumber)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Telegram.Auth/Service/Interface/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using TelegramClient.Auth.Domain;
+
+namespace TelegramClient.Auth.Service.Interface;
+
+public interface IAuthService
+{
+    public User Login(string phoneNumber, string password);
+    public void Registration(string name, string password, string phoneNumber);
+}
