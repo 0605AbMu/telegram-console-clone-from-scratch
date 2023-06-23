8c9a49ad0f99fa061eba2c9e1a88af5a3d3739a4
Malika Rasulova <malika-rasulova-2000@mail.ru>
2023-06-23 19:25:36 +0500

    Creat Display Class

---
+++ Telegregram.Client/Display.cs
@@ -0,0 +1,21 @@
+using TelegramClient.Auth.UI;
+
+namespace Telegregram.Client;
+
+public class Display
+{
+    private Context _contextAuth ;
+
+    public Display()
+    {
+        this._contextAuth = new Context();
+    }
+
+    public void RevreshScreen()
+    {
+        while (true)
+        {
+            //this._context.ActiwView.Start();
+        }
+    }
+}
