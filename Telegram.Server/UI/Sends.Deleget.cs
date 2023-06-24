32fe7290f09bc1f2198356e8eef32cb0a6c94af7
Ilhom75 <ilhomjona692@gmail.com>
2023-06-24 19:50:39 +0500

    deleget yaratildi

---
+++ Telegram.Server/UI/Sends.Deleget.cs
@@ -0,0 +1,26 @@
+﻿namespace Telegram.Server.UI
+{
+    public class Sends
+    {
+        public delegate void Keys();
+        public Keys KeysEsc;
+        public Keys KeysTab;
+        public Keys KeysEnter;
+        public Keys KeysBackSpace;
+        public void Back()
+        {
+
+        }
+        public void Next()
+        {
+
+        }
+        public void ToSend()
+        {
+
+        }
+        public void DeleteMessenge()
+        {
+        }
+    }
+}
