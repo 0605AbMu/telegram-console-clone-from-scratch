99499bdfd7c37cb735224b4489c35167e58228a0
G'olibjon <golibjonabdurasulov47@gmail.com>
2023-06-24 18:56:30 +0500

    Layout qo'shildi

--- Telegram.Auth/UI/Context.cs
+++ Telegram.Auth/UI/Context.cs
@@ -5,18 +5,10 @@ namespace TelegramClient.Auth.UI;
 
 public class Context
 {
-    private IUserService _userService;
-    public AuthService AuthService;
-    public RegistraionUI RegistraionUi;
-    public ChangePasswordUI ChangePasswordUi;
-    public LoginUI LoginUi;
 
     public Context()
     {
-        LoginUi = new LoginUI();
-        ChangePasswordUi = new ChangePasswordUI();
-        RegistraionUi = new RegistraionUI();
-        AuthService = new AuthService(_userService );
+
         
     }
 }
