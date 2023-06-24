1673247c6d1d935ab56c136f9a4ecf24d8f0b605
Ilhom75 <ilhomjona692@gmail.com>
2023-06-24 23:29:57 +0500

    ChatLoyOut da ChatView ni ichida ChatMessage  yaratildi

---
+++ LayoutChat/UI/ChatView/ChatMessage.cs
@@ -0,0 +1,23 @@
+﻿namespace LayoutChat.UI.ChatView
+{
+    public class ChatMessage
+    {
+
+        private ClientService _clientService;
+        public Guid _chatId { get; set; }
+        public ChatMessage(ClientService clientService)
+        {
+            this._clientService = clientService;
+        }
+        public void Start()
+        {
+            this.Home();
+        }
+        protected virtual void Home()
+        {
+            Console.Clear();
+            if (this._chatId != null)
+                Console.WriteLine(_chatId);
+        }
+    }
+}
